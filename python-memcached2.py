ce186d81dd313689483c83472c403eb3aa2934ef
Sean Reifschneider <jafo@tummy.com>
2013-04-03 06:12:46 -0600

    Adding some basic connection code.

--- python-memcached2.py
+++ python-memcached2.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+
+class ServerConnection:
+	def __init__(self, uri, timeout = None):
+		self.uri = url
+		self.timeout = timeout
+		self.reset()
+
+	def reset(self):
+		self.buffer = ''
+		self.backend = None
+
+	def consume_from_buffer(self, length):
+		data = self.buffer[:length]
+		self.buffer = self.buffer[length:]
+		return data
+
+	def read_until(self, search):
+		start = 0
+		search_len = len(search)
+
+		while True:
+			if self.buffer:
+				pos = self.buffer.find(search, start)
+				if pos >= 0:
+					return self.consume_from_buffer(pos + search_len)
+				else:
+					start = max(0, len(self.buffer) - search_len)
+
+			raise NotImplementedError('Read from backend')
