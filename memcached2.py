f10dafdf187d393ec7e584d67bbefccb1147604e
Sean Reifschneider <jafo@tummy.com>
2013-04-04 00:35:39 -0600

    Adding the abililty to read and write commands.

--- memcached2.py
+++ memcached2.py
@@ -1,20 +1,69 @@
 #!/usr/bin/env python3
 
 
+class Memcached2Exception(Exception):
+	pass
+
+
+class UnknownProtocol(Memcached2Exception):
+	pass
+
+
+class InvalidURI(Memcached2Exception):
+	pass
+
+
 class ServerConnection:
 	def __init__(self, uri, timeout=None):
 		self.uri = uri
+		self.parsed_uri = self.parse_uri()
 		self.timeout = timeout
+		self.backend = None
 		self.reset()
 
 	def reset(self):
-		self.buffer = ''
+		self.buffer = b''
+		if self.backend:
+			self.backend.close()
 		self.backend = None
 
 	def consume_from_buffer(self, length):
 		data = self.buffer[:length]
 		self.buffer = self.buffer[length:]
 		return data
+
+	def parse_uri(self):
+		import re
+		m = re.match(r'memcached://(?P<host>[^:]+)(:(?P<port>[0-9]+))?/',
+				self.uri)
+		if m:
+			group = m.groupdict()
+			port = group.get('port')
+			if not port:
+				port = 11211
+			port = int(port)
+			return {'protocol': 'memcached', 'host': group.get('host'),
+					'port': port}
+
+		raise InvalidURI('Invalid URI: {0}'.format(self.uri))
+
+	def connect(self):
+		if self.backend:
+			return
+
+		self.reset()
+		if self.parsed_uri['protocol'] == 'memcached':
+			import socket
+			self.backend = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.backend.connect((self.parsed_uri['host'],
+					self.parsed_uri['port']))
+			return
+
+		raise UnknownProtocol('Unknown connection protocol: {0}'
+				.format(self.parsed_uri['protocol']))
+
+	def send_command(self, command):
+		self.backend.send(command + b'\n')
 
 	def read_until(self, search):
 		start = 0
@@ -28,4 +77,4 @@ class ServerConnection:
 				else:
 					start = max(0, len(self.buffer) - search_len)
 
-			raise NotImplementedError('Read from backend')
+			self.buffer += self.backend.recv(10000)
--- tests/test_serverconnection.py
+++ tests/test_serverconnection.py
@@ -12,13 +12,42 @@ import unittest
 import sys
 sys.path.insert(0, '..')
 import mctestsupp
+import memcached2
 
 
-class test_Basic(unittest.TestCase):
+class test_ServerConnection(unittest.TestCase):
 	def setUp(self):
 		mctestsupp.flush_local_memcache(self)
 
-	def test_ImportModule(self):
-		import memcached2
+	def test_URI(self):
+		with self.assertRaises(memcached2.InvalidURI):
+			memcached2.ServerConnection('failure://host.name/')
+
+		sc = memcached2.ServerConnection('memcached://localhost/')
+		self.assertEqual(sc.parsed_uri['port'], 11211)
+		self.assertEqual(sc.parsed_uri['host'], 'localhost')
+		self.assertEqual(sc.parsed_uri['protocol'], 'memcached')
+
+		sc = memcached2.ServerConnection('memcached://127.0.0.1/')
+		self.assertEqual(sc.parsed_uri['port'], 11211)
+		self.assertEqual(sc.parsed_uri['host'], '127.0.0.1')
+		self.assertEqual(sc.parsed_uri['protocol'], 'memcached')
+
+		sc = memcached2.ServerConnection('memcached://localhost:11234/')
+		self.assertEqual(sc.parsed_uri['port'], 11234)
+		self.assertEqual(sc.parsed_uri['host'], 'localhost')
+		self.assertEqual(sc.parsed_uri['protocol'], 'memcached')
+
+		sc = memcached2.ServerConnection('memcached://127.0.0.1:11234/')
+		self.assertEqual(sc.parsed_uri['port'], 11234)
+		self.assertEqual(sc.parsed_uri['host'], '127.0.0.1')
+		self.assertEqual(sc.parsed_uri['protocol'], 'memcached')
+
+	def test_BasicCommand(self):
+		sc = memcached2.ServerConnection('memcached://localhost/')
+		sc.connect()
+		sc.send_command(b'flush_all')
+		self.assertEqual(sc.read_until(b'\r\n'), b'OK\r\n')
+		sc.reset()
 
 unittest.main()
