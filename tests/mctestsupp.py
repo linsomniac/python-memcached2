c5f21f584af9a9422d7ee8a1a4ceeceb460c3cb1
Sean Reifschneider <jafo@tummy.com>
2013-04-07 04:07:21 -0600

    Moving Fake memcache server into test supp code.

--- tests/mctestsupp.py
+++ tests/mctestsupp.py
@@ -9,3 +9,33 @@ def flush_local_memcache(test):
     results = s.recv(1000)
     test.assertIn(b'OK', results)
     s.close()
+
+
+class FakeMemcacheServer:
+    '''A simple socket server so that specific error conditions can be tested.
+    This must be subclassed and implment the "server()" method.'''
+
+    def __init__(self):
+        import socket
+        import os
+        import signal
+
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.listen(1)
+        self.port = self.s.getsockname()[1]
+        self.pid = os.fork()
+
+        if self.pid != 0:
+            self.s.close()
+            del self.s
+        else:
+            def alarm(signum, frame):
+                os._exit(0)
+
+            count = 0
+            signal.signal(signal.SIGALRM, alarm)
+            signal.alarm(5)
+            while True:
+                conn, addr = self.s.accept()
+                self.server(self.s, conn, count)
+                count += 1
--- tests/test_serverconnection.py
+++ tests/test_serverconnection.py
@@ -15,36 +15,6 @@ import mctestsupp
 import memcached2
 
 
-class FakeMemcacheServer:
-    '''A simple socket server so that specific error conditions can be tested.
-    This must be subclassed and implment the "server()" method.'''
-
-    def __init__(self):
-        import socket
-        import os
-        import signal
-
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.listen(1)
-        self.port = self.s.getsockname()[1]
-        self.pid = os.fork()
-
-        if self.pid != 0:
-            self.s.close()
-            del self.s
-        else:
-            def alarm(signum, frame):
-                os._exit(0)
-
-            count = 0
-            signal.signal(signal.SIGALRM, alarm)
-            signal.alarm(5)
-            while True:
-                conn, addr = self.s.accept()
-                self.server(self.s, conn, count)
-                count += 1
-
-
 class test_ServerConnection(unittest.TestCase):
     def setUp(self):
         mctestsupp.flush_local_memcache(self)
@@ -81,7 +51,7 @@ class test_ServerConnection(unittest.TestCase):
         sc.reset()
 
     def test_ServerFlushDisconnect(self):
-        class DisconnetAfterCommandServer(FakeMemcacheServer):
+        class DisconnetAfterCommandServer(mctestsupp.FakeMemcacheServer):
             def server(self, sock, conn, count):
                 conn.recv(100)
                 conn.close()
@@ -96,7 +66,7 @@ class test_ServerConnection(unittest.TestCase):
             self.assertEqual(sc.read_until(b'\r\n'), b'OK\r\n')
         sc.reset()
 
-        class ImmediatelyDisconnectServer(FakeMemcacheServer):
+        class ImmediatelyDisconnectServer(mctestsupp.FakeMemcacheServer):
             def server(self, sock, conn, count):
                 conn.close()
 
